ac39559b0c6c56346f35c9a08c56a4a8b3c231e5
vien3440 <viendev34.com>
2018-03-02 21:44:17 +0100

    Implementation userbundle & configuration security.yml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,6 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+    
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,107 +1,27 @@
 <?php
+// src/AppBundle/Entity/User.php
 
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user", uniqueConstraints={@ORM\UniqueConstraint(name="pass_UNIQUE", columns={"pass"}), @ORM\UniqueConstraint(name="id_UNIQUE", columns={"id"})})
  * @ORM\Entity
+ * @ORM\Table(name="user")
  */
-class User
+class User extends BaseUser
 {
     /**
-     * @var string
-     *
-     * @ORM\Column(name="nameUser", type="string", length=45, nullable=true)
-     */
-    private $nameuser;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="pseudo", type="string", length=45, nullable=false)
-     */
-    private $pseudo;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="pass", type="string", length=45, nullable=false)
-     */
-    private $pass;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=45, nullable=false)
-     */
-    private $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="imgUser", type="string", length=45, nullable=true)
-     */
-    private $imguser;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    function getNameuser() {
-        return $this->nameuser;
-    }
-
-    function getPseudo() {
-        return $this->pseudo;
-    }
-
-    function getPass() {
-        return $this->pass;
-    }
-
-    function getEmail() {
-        return $this->email;
-    }
-
-    function getImguser() {
-        return $this->imguser;
-    }
-
-    function getId() {
-        return $this->id;
-    }
-
-    function setNameuser($nameuser) {
-        $this->nameuser = $nameuser;
-    }
-
-    function setPseudo($pseudo) {
-        $this->pseudo = $pseudo;
-    }
-
-    function setPass($pass) {
-        $this->pass = $pass;
-    }
-
-    function setEmail($email) {
-        $this->email = $email;
-    }
-
-    function setImguser($imguser) {
-        $this->imguser = $imguser;
-    }
-
+    protected $id;
  
-
-
+    public function __construct()
+    {
+        
+        parent::__construct();
+    }
 }
-
